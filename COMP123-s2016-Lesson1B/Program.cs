ddaee3ae7d5901736d24abee02acdc8bcf821bcf
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-05-13 10:50:34 -0400

    Initial COmmit

---
+++ COMP123-s2016-Lesson1B/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+* Author: Meer Zaheen
+* Date Created: May 13, 2016
+* Date Modified: May 14, 2016
+* Student ID: 300522487
+* 
+* Description: This program demonstrates GitHUB and version control 
+* best practices, we also explore unit testing
+*
+* Version: 0.1 - Initial Version 
+*/
+
+namespace COMP123_s2016_Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
